39c0524099662b3037162efe5ce1e0321ead3821
João Vitor Regis <joaovitorq69@gmail.com>
2026-08-28 10:06:36 -0300

    Update Exercício-20.py

--- Atividades/Exercício-20.py
+++ Atividades/Exercício-20.py
@@ -1,11 +1,13 @@
+# Verificação de dados de um aluno
+
 Nome = input('Digite seu nome: ')
 Idade = int(input('Digite sua idade: '))
 Nota = int(input('Digite sua nota: '))
 if Nota >= 7 and Idade >= 18:
-    print(f'Nome: {Nome}\n idade: {Idade}\n Situação: Maior de Idade\n Resultado: Aprovado ')
+    print(f' Nome: {Nome}\n idade: {Idade}\n Situação: Maior de Idade\n Resultado: Aprovado ')
 if Nota >= 7 and Idade < 18:
-    print(f'Nome: {Nome}\n idade: {Idade}\n Situação: Menor de Idade\n Resultado: Aprovado ')
+    print(f' Nome: {Nome}\n idade: {Idade}\n Situação: Menor de Idade\n Resultado: Aprovado ')
 if Nota < 7 and Idade < 18:
-    print(f'Nome: {Nome}\n idade: {Idade}\n Situação: Menor de Idade\n Resultado: Reprovado ')
+    print(f' Nome: {Nome}\n idade: {Idade}\n Situação: Menor de Idade\n Resultado: Reprovado ')
 if Nota < 7 and Idade >= 18:
-    print(f'Nome: {Nome}\n idade: {Idade}\n Situação: Maior de Idade\n Resultado: Reprovado ')
+    print(f' Nome: {Nome}\n idade: {Idade}\n Situação: Maior de Idade\n Resultado: Reprovado ')
