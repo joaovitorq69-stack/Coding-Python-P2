c6e952289ee62f82cfd887aa0de4e33fa893e62a
João Vitor Regis <joaovitorq69@gmail.com>
2026-08-28 17:16:41 -0300

    Update Exercício-20.py

--- Atividades/Exercício-20.py
+++ Atividades/Exercício-20.py
@@ -5,9 +5,13 @@ Idade = int(input('Digite sua idade: '))
 Nota = int(input('Digite sua nota: '))
 if Nota >= 7 and Idade >= 18:
     print(f' Nome: {Nome}\n idade: {Idade}\n Situação: Maior de Idade\n Resultado: Aprovado ')
-if Nota >= 7 and Idade < 18:
+elif Nota >= 7 and Idade < 18:
     print(f' Nome: {Nome}\n idade: {Idade}\n Situação: Menor de Idade\n Resultado: Aprovado ')
-if Nota < 7 and Idade < 18:
+elif Nota < 7 and Idade < 18:
     print(f' Nome: {Nome}\n idade: {Idade}\n Situação: Menor de Idade\n Resultado: Reprovado ')
-if Nota < 7 and Idade >= 18:
+else Nota < 7 and Idade >= 18:
     print(f' Nome: {Nome}\n idade: {Idade}\n Situação: Maior de Idade\n Resultado: Reprovado ')
+
+    # Explicação: 
+    # Primeiro peço o nome do usuário, sua idade e a nota
+    # Em seguida 
