6fe03161a91771b0b86bbc391e496de1c7491895
João Vitor Regis <joaovitorq69@gmail.com>
2026-09-01 19:44:02 -0300

    atualização

--- Atividades/Exercício-20.py
+++ Atividades/Exercício-20.py
@@ -5,13 +5,14 @@ Idade = int(input('Digite sua idade: '))
 Nota = int(input('Digite sua nota: '))
 if Nota >= 7 and Idade >= 18:
     print(f' Nome: {Nome}\n idade: {Idade}\n Situação: Maior de Idade\n Resultado: Aprovado ')
-elif Nota >= 7 and Idade < 18:
+if Nota >= 7 and Idade < 18:
     print(f' Nome: {Nome}\n idade: {Idade}\n Situação: Menor de Idade\n Resultado: Aprovado ')
-elif Nota < 7 and Idade < 18:
+if Nota < 7 and Idade < 18:
     print(f' Nome: {Nome}\n idade: {Idade}\n Situação: Menor de Idade\n Resultado: Reprovado ')
-else Nota < 7 and Idade >= 18:
+if Nota < 7 and Idade >= 18:
     print(f' Nome: {Nome}\n idade: {Idade}\n Situação: Maior de Idade\n Resultado: Reprovado ')
 
     # Explicação: 
     # Primeiro peço o nome do usuário, sua idade e a nota
-    # Em seguida 
+    # Em seguida é feita uma verificação de parâmetros Utilizando if´s
+    # Após essa verificação o programa mostra o resultado de acordo de acordo com as informações dadas
--- Atividades/Exercírcio-18.py
+++ Atividades/Exercírcio-18.py
@@ -3,3 +3,7 @@ if n % 5 == 0:
     print('Esse número é divisível por 5')
 else: 
     print('Esse número não é divisível por 5')
+
+    # É solicitado ao usuário um Número qualquer
+    # O programa vai ver se esse número é divisível por 5
+    # E por fim mostrar na tela o resultado
